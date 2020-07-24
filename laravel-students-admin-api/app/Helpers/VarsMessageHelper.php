84ce6654733477198e2d85a57ae2cbc8d14401a7
unknown <guilhermealves2596@gmail.com>
2020-07-24 20:43:57 -0300

    Adding Helper class for standard messages

---
+++ laravel-students-admin-api/app/Helpers/VarsMessageHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Helpers;
+
+class VarsMessageHelper
+{
+    public static $okMessage = "Retrieved successfully";
+    public static $createdMessage = "Created successfully";
+    public static $deletedMessage = "Deleted successfully";
+    public static $notFoundMessage = "Resource not found";
+    public static $badRequestMessage = "Bad Request! Check structure and/or parameters"; 
+    public static $nullParameterMessage = "Null parameter detected";
+    public static $unprocessableEntityMessage = "Entity unprocessed in the request";
+    public static $existsEnrollmentMessage = "This course and student information already exists in an existing enrollment!";
+    
+}
