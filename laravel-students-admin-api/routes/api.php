20a5b4e5019150482cef88c8ef97fb356572cfc2
unknown <guilhermealves2596@gmail.com>
2020-07-24 22:48:18 -0300

    Send .env and update .gitignore

--- laravel-students-admin-api/routes/api.php
+++ laravel-students-admin-api/routes/api.php
@@ -17,26 +17,29 @@ use App\Http\Resources\StudentsResource;
 |
 */
 
-Route::get('/students', 'StudentsController@get');
-Route::get('/students/{id}', 'StudentsController@getById');
-Route::get('/students/name/{name}', 'StudentsController@getByName');
-Route::get('/students/enrollments/{id}', 'StudentsController@getEnrollmentsByStudent');
-Route::get('/students/email/{email}', 'StudentsController@getByEmail');
-Route::post('/students', 'StudentsController@store');
-Route::put('/students/{id}', 'StudentsController@update');
-Route::delete('/students/{id}', 'StudentsController@delete');
+Route::group([ 'prefix' => env('API_VERSION')], function () {
+   
+    Route::get('/students', 'StudentsController@get');
+    Route::get('/students/{id}', 'StudentsController@getById');
+    Route::get('/students/name/{name}', 'StudentsController@getByName');
+    Route::get('/students/enrollments/{id}', 'StudentsController@getEnrollmentsByStudent');
+    Route::get('/students/email/{email}', 'StudentsController@getByEmail');
+    Route::post('/students', 'StudentsController@store');
+    Route::put('/students/{id}', 'StudentsController@update');
+    Route::delete('/students/{id}', 'StudentsController@delete');
 
-Route::get('/courses', 'CoursesController@get');
-Route::get('/courses/{id}', 'CoursesController@getById');
-Route::get("/courses/enrollments/{id}", 'CoursesController@getEnrollmentsByCourse');
-Route::get('/courses/title/{title}', 'CoursesController@getByTitle');
-Route::post('/courses', 'CoursesController@store');
-Route::put('/courses/{id}', 'CoursesController@update');
-Route::delete('/courses/{id}', 'CoursesController@delete');
+    Route::get('/courses', 'CoursesController@get');
+    Route::get('/courses/{id}', 'CoursesController@getById');
+    Route::get("/courses/enrollments/{id}", 'CoursesController@getEnrollmentsByCourse');
+    Route::get('/courses/title/{title}', 'CoursesController@getByTitle');
+    Route::post('/courses', 'CoursesController@store');
+    Route::put('/courses/{id}', 'CoursesController@update');
+    Route::delete('/courses/{id}', 'CoursesController@delete');
 
-Route::get('/enrollments', 'EnrollmentsController@get');
-Route::get('/enrollments/{id}', 'EnrollmentsController@getById');
-Route::post('/enrollments', 'EnrollmentsController@store');
-Route::put('/enrollments/{id}', 'EnrollmentsController@update');
-Route::delete('/enrollments/{id}', 'EnrollmentsController@delete');
+    Route::get('/enrollments', 'EnrollmentsController@get');
+    Route::get('/enrollments/{id}', 'EnrollmentsController@getById');
+    Route::post('/enrollments', 'EnrollmentsController@store');
+    Route::put('/enrollments/{id}', 'EnrollmentsController@update');
+    Route::delete('/enrollments/{id}', 'EnrollmentsController@delete');
 
+});
